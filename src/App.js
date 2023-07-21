6d915a7aa8f7a60c7f22a995b7b7112cbff5af9d
smbe-systems <smbe.systems@gmail.com>
2023-07-21 14:16:51 +0800

    Intial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './seed';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class ProductList extends React.Component { 
+  state = {
+    products : []
+  }; 
+     
+  componentDidMount(){
+    this.setState({products : Seed.products})
+  }
+
+  handleUpVote = (productId) =>{
+    let nextProducts = this.state.products.map((product) => {
+      if(product.id == productId){
+        return Object.assign({}, product, {
+          votes : product.votes + 1
+        })
+      }else{
+        return product;
+      }
+    })
+    this.setState({
+      products : nextProducts
+    });
+  }
+
+  render() {
+    const products = this.state.products.sort((a, b) => (
+      b.votes - a.votes
+    ));
+
+    const productComponents = products.map((product) => (
+      <Product
+        key={'product-' + product.id}
+        id={product.id}
+        title={product.title}
+        description={product.description}
+        url={product.url}
+        votes={product.votes}
+        submitterAvatarUrl={product.submitterAvatarUrl}
+        productImageUrl={product.productImageUrl}
+        onVote={this.handleUpVote}
+      />
+    ));
+    return (
+      <div className='ui unstackable items'>
+        {productComponents}
+      </div>
+    );
+  }
 }
 
-export default App;
+class Product extends React.Component { 
+
+  handleUpVote = () =>{
+    this.props.onVote(this.props.id)
+  }
+
+  render() {
+    return (
+      <div className='item'>
+        <div className='image'>
+          <img src={this.props.productImageUrl} />
+        </div>
+        <div className='middle aligned content'>
+          <div className='header'>
+            <a onClick={this.handleUpVote}>
+              <i className='large caret up icon' />
+            </a>
+            {this.props.votes}
+          </div>
+          <div className='description'>
+            <a href={this.props.url}>
+              {this.props.title}
+            </a>
+            <p>
+              {this.props.description}
+            </p>
+          </div>
+          <div className='extra'>
+            <span>Submitted by:</span>
+            <img
+              className='ui avatar image'
+              src={this.props.submitterAvatarUrl}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+} 
+
+export default ProductList;
